6a5a6bb03d16468afa910aeab251cce8ca2fd192
Collin Moffatt <cmoffatt3@huskers.unl.edu>
2020-08-23 17:24:58 +0000

    Added Collins info

--- hello.c
+++ hello.c
@@ -7,8 +7,14 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+
 int main( int argc, char **argv) {
+    
     printf("Ian Gordon!\n");
     printf("Major: Computer Science\n");
+    
+    printf("Collin Moffatt!\n");
+    printf("Computer Science\n");
+    
     return 0;
 }
